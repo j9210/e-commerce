74c086c7e39e51a19f6afdc21cd24bc65d810605
j9210 <71297542+j9210@users.noreply.github.com>
2021-02-04 18:35:15 -0600

    install npm pckgs

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -5,9 +5,7 @@ const sequelize = require('../config/connection.js');
 class Category extends Model {}
 
 Category.init(
-  {
-    // define columns
-  },
+
   {
     sequelize,
     timestamps: false,
